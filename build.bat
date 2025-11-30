cdabb3d85a9289ec5c7bd977c8788050d241bf8b
Jordan Rahim <enderbyte09@gmail.com>
2025-11-29 19:42:27 -0800

    build improvements

--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
-pyinstaller --icon=icon.ico -n map -w main.py
+pyinstaller --icon=icon.ico -n map -w --no-confirm main.py
+copy icon.png dist\map\icon.png
+copy icon.ico dist\map\icon.ico
+copy config.json dist\map\config.json
+xcopy assets\ dist\map\assets\ /E /-Y
